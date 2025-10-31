dbc0b7b19fa01ae494a117e6539d6abef4f4d373
Rohan Das <rohandas8944@gmail.com>
2025-10-31 17:28:02 +0530

    add

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, Toast],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [MessageService]
 })
 export class App {
   protected readonly title = signal('my-angular20-app');
+  constructor(private messageService: MessageService) {}
+
+    show() {
+        this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 });
+    }
 }
