57a886194c6f4208f99aac6d447d9a79133fa0b4
PSpabitra <sarkarpabi42@gmail.com>
2025-12-23 12:52:10 +0530

    ADDED

--- src/app/components/new-design/create-third-design/create-third-design.ts
+++ src/app/components/new-design/create-third-design/create-third-design.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './create-third-design.html',
 })
 export class CreateThirdDesignComponent {
+  @Input() designType: number = 1;
+
+  get assets() {
+    const defaults = {
+      logo: '/image 504.svg',
+      plane: '/6E.svg',
+    };
+    const configs: any = {
+      1: defaults,
+      2: { logo: '/image 507.svg', plane: '/IX.svg',  },
+      3: { logo: '/image 506.svg', plane: '/QP.svg', },
+      4: { logo: '/Star Air.svg', plane: '/S5.svg'},
+    };
+    return configs[this.designType] || defaults;
+  }
+
   confetti = Array.from({ length: 70 }).map(() => ({
     x: Math.random() * 100,
     delay: Math.random() * 2,
